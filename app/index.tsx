bd8b29a67d780f01c53118236a61aab68c216f70
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2025-04-15 15:06:25 +0530

    add the file.

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <Text
+        style={{
+          backgroundColor: "blue",
+          textShadowColor: "red"
+        }}
+      >Hello Deepak, Welcome to Native!</Text>
+    </View>
+  );
+}
