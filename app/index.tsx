125d91e76789b4fe595aec9252335e596442641c
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2025-04-15 15:31:07 +0530

    modified the file.

--- app/index.tsx
+++ app/index.tsx
@@ -12,7 +12,6 @@ export default function Index() {
       <Text
         style={{
           backgroundColor: "blue",
-          textShadowColor: "red"
         }}
       >Hello Deepak, Welcome to Native!</Text>
     </View>
