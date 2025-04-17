beafe47c82e5ba0199dda6130d59e09732f9734a
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2025-04-17 12:44:48 +0530

    add the markets

--- app/index.tsx
+++ app/index.tsx
@@ -2,18 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}
-    >
-      <Text
-        style={{
-
-        }}
-      >Hello Deepak, Welcome to Native!</Text>
+    <View>
+      <Text>Markets</Text>
     </View>
   );
 }
